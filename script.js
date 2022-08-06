d6426a9efbcfb2f5c8ab31ee970ea08eee03a345
Thomas <tho.daskalakis@gmail.com>
2022-08-06 18:20:06 +0300

    Link events to buttons. Complete operate()
    Display now responds to button clicks.
    
    Display now has a small padding.
    
    Also completed the operate function.

--- script.js
+++ script.js
@@ -9,5 +9,30 @@ function multiply(a, b) { return a * b; }
 function divide(a, b) { return a / b; }
 
 function operate(operator, a, b) {
-    if (operator === '+') { return add(a, b); }
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case 'x':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+    }
 }
+
+// Calculator
+const calculator = document.querySelector('#calculator');
+console.log(calculator);
+
+const display = calculator.querySelector('#display');
+console.log(display);
+
+const buttons = [...calculator.querySelectorAll('#buttons button')];
+console.log(buttons);
+
+// Button funcionality
+
+buttons.forEach(button => button.addEventListener('click', e => {
+    display.textContent = e.target.textContent;
+}));
