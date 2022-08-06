37a294faa521565def9e3762df660fa85c481335
Thomas <tho.daskalakis@gmail.com>
2022-08-06 20:24:12 +0300

    Make calculator fully functional

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Calculator functions
 
-function add(a, b) { return a + b;}
+function add(a, b) { return parseFloat(a) + parseFloat(b);}
 
 function subtract(a, b) { return a - b;}
 
@@ -25,12 +25,67 @@ function operate(operator, a, b) {
 // Calculator
 const calculator = document.querySelector('#calculator');
 
+let cachedNumber = null;
+
+
 // Display
 const display = calculator.querySelector('#display');
 
 const DISPLAY_LENGTH = 13;
 
+const cachedDisplay = calculator.querySelector('#cached');
+
+function updateCached(value) {
+    cachedDisplay.textContent = value;
+}
+
 // Button funcionality
+
+// Clear
+const clear = calculator.querySelector('#clear');
+
+clear.addEventListener('click', e => {
+    display.textContent = '0';
+});
+
+// Dot
+const dot = calculator.querySelector('#dot');
+
+dot.addEventListener('click', e => {
+    if (!display.textContent.includes('.')) {
+        display.textContent += '.';
+    }
+});
+
+// Operations
+const operators = [...calculator.querySelectorAll('.operator')];
+
+let operator = null;
+
+operators.forEach(oper => oper.addEventListener('click', e => {
+    if (display.textContent !== '0') {
+        operator = e.target.textContent;
+        
+        cachedNumber = display.textContent;
+
+        updateCached(cachedNumber + operator);
+
+        display.textContent = '0';
+    }
+}));
+
+// Equals
+const equals = calculator.querySelector('#equals');
+
+equals.addEventListener('click', e => {
+    if (operator !== null) {
+        display.textContent = 
+            operate(operator, cachedNumber, display.textContent);
+        reset();
+        }
+    });
+
+// Digits
 const digits = [...calculator.querySelectorAll('#buttons button.digit')];
 
 digits.forEach(digit => digit.addEventListener('click', e => {
@@ -49,6 +104,16 @@ digits.forEach(digit => digit.addEventListener('click', e => {
 
 function init() {
     display.textContent = '0';
+    cachedDisplay.textContent = '0';
+    operator = null;
 }
 
 init();
+
+// Reset after equals
+
+function reset() {
+    operator = null;
+    cachedNumber = 0;
+    cachedDisplay.textContent = cachedNumber;
+}
