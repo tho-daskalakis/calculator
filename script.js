bfff782da1b75d57877963409c4e95ca608a6bca
Thomas <tho.daskalakis@gmail.com>
2022-08-06 16:05:02 +0300

    Add basic calculator interface
    Add a calculator display with background color.
    
    Add calculator buttons.
    
    Apply a calculator font on calculator elements.

---
+++ script.js
@@ -0,0 +1,13 @@
+// Calculator functions
+
+function add(a, b) { return a + b;}
+
+function subtract(a, b) { return a - b;}
+
+function multiply(a, b) { return a * b; }
+
+function divide(a, b) { return a / b; }
+
+function operate(operator, a, b) {
+    if (operator === '+') { return add(a, b); }
+}
